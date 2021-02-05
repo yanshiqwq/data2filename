7b7a125b9c13d3ebc411c77ec9609711a0ba86a2
Yanshiqwq <yanshiqwq@github.com>
2021-02-05 18:59:58 +0800

    modified:   README.md new file:   base64.exe modified:   decodefile.bat modified:   decodefile.exe modified:   encodefile.bat modified:   encodefile.exe

--- decodefile.bat
+++ decodefile.bat
@@ -1,47 +1,85 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdRQguUM1eeA6YX/Ofr0+6CoUIZUeQ2dIqV36yLQA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 echo.
-echo %~nx0 By 延时qwq https://space.bilibili.com/431304449
+echo %~nx0 v1.1 By 延时qwq https://space.bilibili.com/431304449
 echo.
 set RAND=%RANDOM%
 for /f %%i in ('where /r "%~1" index_*') do (set INDEXFILE=%%~ni)
-for /f %%i in ('where /r "%~1" filename_*') do (set NAMEFILE=%%~nxi)
+for /f %%i in ('where /r "%~1" filename_*') do (set NAMEFILE=%%~ni)
 for /f "delims=_ tokens=2" %%i  in ('echo %NAMEFILE%') do (set FILENAME=%%i)
+for /f "delims=._ tokens=3" %%i  in ('echo %NAMEFILE%') do (set FILEEXT=%%i)
+if "%FILEEXT%" NEQ "~1" (
+	set FILENAME=%FILENAME%.%FILEEXT%
+)
 for /f "delims=_ tokens=2,3" %%i in ('echo %INDEXFILE%') do (set FILERAND=%%i)
 for /f "delims=_ tokens=3" %%i in ('echo %INDEXFILE%') do (set FILECOUNT=%%i)
-echo 还原速度:	约7KB/s
-if "%1" == "" echo 用法: %~nx0 待解密的文件 & goto :EOF
+echo 还原速度:	约79KB/s
+if "%~1" == "" (
+	echo 用法: %~nx0 待解密的文件
+	goto :EOF
+)
 echo 文件名称:	%FILENAME%
-set /a COMPILETIME=%~z1/7000
-echo 约需时间:	%COMPILETIME%秒
-echo [INFO] 正在合并文件...
-echo -----BEGIN CERTIFICATE----- >> .\filedata%RAND%.tmp
+set /a GB=%~z1/1024/1024/1024
+set /a MB=%~z1/1024/1024%%1024
+set /a KB=%~z1/1024%%1024
+echo 文件大小:	%GB%GB %MB%MB %KB%KB
+set /a FILESIZE=%~z1/1024
+set /a COMPLETESEC=%FILESIZE%/79%%60
+set /a COMPLETEMIN=%FILESIZE%/79/60%%60
+set /a COMPLETEHOUR=%FILESIZE%/79/3600%%60
+echo 约需时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 
-set COUNT=0
-:loop
+echo [INFO] 正在合并文件...
 setlocal enabledelayedexpansion
-for /f "delims=_ tokens=4" %%i in ('where /r "%~1" /f data%FILERAND%_%COUNT%_*') do (
+for /f "delims=_ tokens=3" %%i in ('dir /b /od /on /oe "%~1\*."') do (
 	set FILEDATA=%%i
 	set FILEDATA=!FILEDATA:-=/!
-	set FILEDATA=!FILEDATA:"=!
-	echo !FILEDATA! >> .\filedata%RAND%.tmp
+	echo !FILEDATA! >> %~dp1file%RAND%.tmp
 )
-if "%COUNT%"=="%FILECOUNT%" endlocal & goto :end
 endlocal
-set /a COUNT=%COUNT%+1
-goto :loop
-:end
-echo -----END CERTIFICATE----- >> .\filedata%RAND%.tmp
 
 echo [INFO] 合并完成!正在解码文件...
-certutil -decode .\filedata%RAND%.tmp .\%FILENAME% >nul
-del /f /q .\filedata%RAND%.tmp >nul
+base64 -di %~dp1file%RAND%.tmp > %~dp1%FILENAME%
+del /f /q %~dp1file%RAND%.tmp >nul
 
 echo [INFO] 解码完成!正在校验文件哈希值...
 for /f %%i in ('where /r "%~1" hash_*') do (set HASHFILE=%%~ni)
 set FILEHASH=%HASHFILE:~5%
-certutil -hashfile .\%FILENAME% .\hash%RAND%.tmp MD5
-for /f %%i in ('type .\hash%RAND%.tmp') do (set HASH=%%i)
-del /f /q .\hash%RAND%.tmp >nul
+certutil -hashfile %~dp1%FILENAME% MD5 | findstr /V 哈希 | findstr /V 完成 > %~dp1hash%RAND%.tmp
+for /f %%i in ('type %~dp1hash%RAND%.tmp') do (set HASH=%%i)
+del /f /q %~dp1hash%RAND%.tmp >nul
 if "%HASH%" == "%FILEHASH%" (
 	echo [INFO] 校验完成!
 	goto :EOF
--- encodefile.bat
+++ encodefile.bat
@@ -1,38 +1,76 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdRQguUM1eeA6YX/Ofr0++JoUIZUeQ2dIqV36yLQA==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 echo.
-echo         %~nx0 By 延时qwq https://space.bilibili.com/431304449
+echo         %~nx0 v1.1 By 延时qwq https://space.bilibili.com/431304449
 echo.
-echo 生成速度:	约33KB/s
+echo 生成速度:	约40KB/s
 echo 生成数量:	约21文件/KB
-if "%1" == "" echo 用法: %~nx0 待加密的文件 & goto :EOF
+if "%~1" == "" echo 用法: %~nx0 待加密的文件 & goto :EOF
 echo 文件名称:	%~nx1
-set /a FILESIZE=%~z1/1000
-echo 文件大小:	%FILESIZE%KB
-set /a COMPILETIME=%~z1/33000
-echo 约需时间:	%COMPILETIME%秒
+set /a GB=%~z1/1024/1024/1024
+set /a MB=%~z1/1024/1024%%1024
+set /a KB=%~z1/1024%%1024
+echo 文件大小:	%GB%GB %MB%MB %KB%KB
+set /a FILESIZE=%~z1/1024
+set /a COMPLETESEC=%FILESIZE%/40%%60
+set /a COMPLETEMIN=%FILESIZE%/40/60%%60
+set /a COMPLETEHOUR=%FILESIZE%/40/3600%%60
+echo 约需时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 set RAND=%RANDOM%
-set FILENAME=%~nx1
-for /f "delims=" %%i in ('certutil -encode "%~1" .\file%RAND%.tmp ^| findstr /c:"输出长度 ="') do (set OUTSIZE=%%i)
-set /a OUTSIZE=%OUTSIZE:~7%/1024
-echo 输出大小:	%OUTSIZE%KB
-mkdir %FILENAME%_encode >nul
+base64 "%~1" > "%~dp1file%RAND%.tmp"
+set OUTFILE=%~nx1_encode
+set OUTFILE=%OUTFILE: =_%
+mkdir "%~dp1%OUTFILE%" >nul
 
 echo [INFO] 正在计算文件哈希值...
-for /f "delims=" %%i in ('certutil -hashfile %1 MD5 ^| findstr /V 哈希 ^| findstr /V 完成') do (cd. > .\%FILENAME%_encode\hash_%%i) 
+for /f "delims=" %%i in ('certutil -hashfile %1 MD5 ^| findstr /V 哈希 ^| findstr /V 完成') do (cd. > "%~dp1%OUTFILE%\hash_%%i.encode") 
 
 echo [INFO] 正在分解文件...
-setlocal enabledelayedexpansion
 set COUNT=0
-for /f "skip=1 delims=" %%i in ('type .\file%RAND%.tmp') do (
+for /f "delims=" %%i in ('type "%~dp1file%RAND%.tmp"') do (
+	setlocal enabledelayedexpansion
 	set FILEDATA=%%i
 	set FILEDATA=!FILEDATA:/=-!
-	cd. > ".\%FILENAME%_encode\data%RAND%_!COUNT!_!FILEDATA!"
-	set /a COUNT=!COUNT!+1
+	cd. > "%~dp1%OUTFILE%\data%RAND%_!COUNT!_!FILEDATA!"
+	endlocal
+	set /a COUNT+=1
 )
+
+del /f /q "%~dp1file%RAND%.tmp" >nul
 set /a COUNT=%COUNT%-1
-del /f /q ".\%FILENAME%_encode\data%RAND%_%COUNT%_-----END CERTIFICATE-----" >nul
-del /f /q .\file%RAND%.tmp >nul
-set /a COUNT=%COUNT%-1
-cd. > ".\%FILENAME%_encode\index_%RAND%_%COUNT%"
-cd. > ".\%FILENAME%_encode\filename_%FILENAME%"
+cd. > "%~dp1%OUTFILE%\index_%RAND%_%COUNT%.encode"
+set FILEEXT=%~x1
+cd. > "%~dp1%OUTFILE%\filename_%~n1_%FILEEXT:~1%.encode"
 echo [INFO] 分解完成!
