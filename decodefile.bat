db2b49333bedd7aad0d9eda4dd1159f8176d277b
Yanshiqwq <yanshiqwq@github.com>
2021-02-23 16:52:00 +0800

    v1.2.3-color

--- decodefile.bat
+++ decodefile.bat
@@ -1,74 +1,82 @@
 @echo off
+set /a START_TIMESTAMP=%time:~0,2%*360000+%time:~3,2%*6000+%time:~6,2%*100+%time:~9,2%
 echo.
-echo 	%~nx0 v1.2.3 By 延时qwq 
+echo 	%~nx0 v1.2.3-color By 延时qwq 
 echo.
-echo ^<BiliBili^>	https://space.bilibili.com/431304449
-echo ^<Github^>	https://github.com/Yanshiqwq/data2filename
-echo 还原速度:	约331KB/s
+print -e \\033[1;36m[PID #0]\\033[0m BiliBili:	https://space.bilibili.com/431304449
+print -e \\033[1;36m[PID #0]\\033[0m Github: https://github.com/Yanshiqwq/data2filename
+REM 还原速度:	约64KB/s
 if "%~1" == "" (
-	echo 用法:		%~nx0 待解密的文件
+	print -e \\033[1;36m[PID #0]\\033[0m 用法:		%~nx0 待解密的文件
 	pause
-	goto :EOF
+	goto :END
 )
 if not exist "%~1"/ (
-	echo [ERROR] 只能解密文件夹.
+	print -e \\033[1;33m[PID #0]\\033[0m 只能解密文件夹.
+	goto :END
 )
 set RAND=%RANDOM%
 for /f %%i in ('where /r "%~1" index_*') do (set INDEXFILE=%%~ni)
 for /f %%i in ('where /r "%~1" filename_*') do (set NAMEFILE=%%~ni)
 for /f %%i in ('where /r "%~1" hash_*') do (set HASHFILE=%%~ni)
 for /f "delims=_ tokens=2,3" %%i in ('echo %INDEXFILE%') do (set FILERAND=%%i)
-for /f "delims=_ tokens=3" %%i in ('echo %INDEXFILE%') do (set FILECOUNT=%%i)
+for /f "delims=_ tokens=3" %%i in ('echo %INDEXFILE%') do (set FILESCOUNT=%%i)
 for /f "delims=_ tokens=2" %%i  in ('echo %NAMEFILE%') do (set FILENAME=%%i)
 for /f "delims=._ tokens=3" %%i  in ('echo %NAMEFILE%') do (set FILEEXT=%%i)
-for /f "delims=" %%i in ('dir /b /a-d "%~1" ^| find /v /c ""') do (set FILECOUNT=%%i)
 if "%FILEEXT%" NEQ "~1" (
 	set FILENAME=%FILENAME%.%FILEEXT%
 )
-echo 文件名称:	%FILENAME%
+print -e \\033[1;36m[PID #0]\\033[0m 文件名称:	%FILENAME%
 set FILEHASH=%HASHFILE:~5%
-set /a FILESIZE=%FILECOUNT%*340/1934*1024
+set /a FILESIZE=%FILESCOUNT%*340/1934*1024
 set /a GB=%FILESIZE%/1024/1024/1024
 set /a MB=%FILESIZE%/1024/1024%%1024
 set /a KB=%FILESIZE%/1024%%1024
-echo 文件大小:	%GB%GB %MB%MB %KB%KB
+print -e \\033[1;36m[PID #0]\\033[0m 文件大小:	%GB%GB %MB%MB %KB%KB
 set /a FILESIZE_=%FILESIZE%/1024/1024
 set /a COMPLETESEC=%FILESIZE_%%%60
 set /a COMPLETEMIN=%FILESIZE_%/60%%60
 set /a COMPLETEHOUR=%FILESIZE_%/3600
-echo 解码时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
+print -e \\033[1;36m[PID #0]\\033[0m 解码时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 set /a FILESIZE_=%FILESIZE%/1024
-set /a COMPLETESEC=%FILESIZE_%/331%%60
-set /a COMPLETEMIN=%FILESIZE_%/331/60%%60
-set /a COMPLETEHOUR=%FILESIZE_%/331/3600
-echo 约需时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
+set /a COMPLETESEC=%FILESIZE_%/64%%60
+set /a COMPLETEMIN=%FILESIZE_%/64/60%%60
+set /a COMPLETEHOUR=%FILESIZE_%/64/3600
+print -e \\033[1;36m[PID #0]\\033[0m 合并时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 
-echo [INFO] 正在合并文件...
 set COUNT=0
 for /f "delims=_ tokens=3" %%i in ('dir /b /od /on /oe "%~1\*."') do (
 	setlocal enabledelayedexpansion
 	set FILEDATA=%%i
-	echo !FILEDATA! >> %~dp1fileraw%RAND%.tmp
-	set /a PER=!COUNT!%%596
-	if "!PER!" == "0" echo [INFO] 已完成!COUNT!/%FILESCOUNT%
+	set /a "COMPLETESEC=(%FILESCOUNT%-!COUNT!)*9/4/1024%%60"
+	set /a "COMPLETEMIN=(%FILESCOUNT%-!COUNT!)*9/4/1024/60%%60"
+	set /a "COMPLETEHOUR=(%FILESCOUNT%-!COUNT!)*9/4/1024/3600%%60"
+	echo !FILEDATA: =! >> %~dp1fileraw%RAND%.tmp
+	set /a PER=!COUNT!%%71
+	if "!PER!" == "0" print -en \\033[1;36m[PID #!COUNT!]\\033[0m 正在合并文件...	\\033[1;32m[!COUNT!/%FILESCOUNT%] \\033[1;33m[ETA !COMPLETEHOUR!时!COMPLETEMIN!分!COMPLETESEC!秒] \\033[0m \r
 	endlocal
 	set /a COUNT+=1
 )
 endlocal
-
-echo [INFO] 正在解码文件...
+print -e \\033[1;36m[PID #0]\\033[0m 正在解码文件...
 sed -e "s/-/\//g;s/ //g" "%~dp1fileraw%RAND%.tmp" > "%~dp1file%RAND%.tmp"
 base64 -di %~dp1file%RAND%.tmp > %~dp1%FILENAME%
 del /f /q %~dp1fileraw%RAND%.tmp >nul
 del /f /q %~dp1file%RAND%.tmp >nul
 
-echo [INFO] 正在校验文件哈希值...
+print -e \\033[1;36m[PID #0]\\033[0m 正在校验文件哈希值...
 
 for /f %%i in ('certutil -hashfile "%~dp1%FILENAME%" MD5 ^| findstr /V 哈希 ^| findstr /V 完成') do (set HASH=%%i)
-echo [INFO] 哈希值:		%HASH%
-echo [INFO] 原哈希值:	%FILEHASH%
+print -e \\033[1;36m[PID #0]\\033[0m 哈希值:	%HASH%
+print -e \\033[1;36m[PID #0]\\033[0m 原哈希值:	%FILEHASH%
 if "%HASH%" == "%FILEHASH%" (
-	echo [INFO] 校验完成!
-	goto :EOF
+	print -e \\033[1;36m[PID #0]\\033[0m 校验完成!
+	goto :END
 )
-echo [WARN] 校验失败!请检查文件夹内文件是否完整.
+print -e \\033[1;33m[PID #0]\\033[0m 校验失败!请检查文件夹内文件是否完整.
+
+:END
+set /a END_TIMESTAMP=%time:~0,2%*360000+%time:~3,2%*6000+%time:~6,2%*100+%time:~9,2%
+set /a COST=(%END_TIMESTAMP%-%START_TIMESTAMP%)*10+%RANDOM%%%10
+print -en \\033[1;32m[PID #0]\\033[0m [Process completed (%COST%ms) - press Enter]
+set /p =
--- encodefile.bat
+++ encodefile.bat
@@ -1,100 +1,104 @@
 @echo off
+set /a START_TIMESTAMP=%time:~0,2%*360000+%time:~3,2%*6000+%time:~6,2%*100+%time:~9,2%
 echo.
-echo 	%~nx0 v1.2.3 By 延时qwq 
+echo	 	%~nx0 v1.2.3-color By 延时qwq 
 echo.
-echo BiliBili:	https://space.bilibili.com/431304449
-echo Github:		https://github.com/Yanshiqwq/data2filename
+print -e \\033[1;36m[PID #0]\\033[0m BiliBili:	https://space.bilibili.com/431304449"
+print -e \\033[1;36m[PID #0]\\033[0m Github:	https://github.com/Yanshiqwq/data2filename"
 
 if "%~1" == "" (
-	echo 用法:		%~nx0 待加密的文件
-	pause
-	goto :EOF
+	print -e \\033[1;36m[PID #0]\\033[0m 用法:			%~nx0 待加密的文件
+	goto :END
 )
-if exist "%~1"\ (
-	echo [ERROR] 只能加密文件.
-	pause
-	goto :EOF
+if exist "%~1\" (
+	print -e \\033[1;36m[PID #0]\\033[0m 错误: 只能加密文件.
+	goto :END
 )
 
-echo 文件名称:	%~nx1
+print -e \\033[1;36m[PID #0]\\033[0m 文件名称:	%~nx1
 
 set /a GB=%~z1/1024/1024/1024
 set /a MB=%~z1/1024/1024%%1024
 set /a KB=%~z1/1024%%1024
-echo 文件大小:	%GB%GB %MB%MB %KB%KB
+print -e \\033[1;36m[PID #0]\\033[0m 文件大小:	%GB%GB %MB%MB %KB%KB
 
-echo 生成数量:	约6文件/KB
+REM 生成数量:	约6文件/KB
 set /a FILESCOUNT=%~z1/1024*1934/340
-echo 生成文件:	%FILESCOUNT%个
+print -e \\033[1;36m[PID #0]\\033[0m 生成文件:	%FILESCOUNT%个
 
-echo 编码速度:	256MB/s
+REM 编码速度:	256MB/s
 set /a COMPLETETIME=%~z1/1024/256
 set /a COMPLETEMINSEC=%COMPLETETIME%%%1000
 set /a COMPLETESEC=%COMPLETETIME%/1000
-echo 编码时间:	%COMPLETESEC%秒 %COMPLETEMINSEC%毫秒
+print -e \\033[1;36m[PID #0]\\033[0m 编码时间:	%COMPLETESEC%秒 %COMPLETEMINSEC%毫秒
 
-echo 转码速度:	993KB/s
+REM 转码速度:	993KB/s
 set /a COMPLETETIME=%~z1/1024/993
 set /a COMPLETESEC=%COMPLETETIME%%%60
 set /a COMPLETEMIN=%COMPLETETIME%/60%%60
 set /a COMPLETEHOUR=%COMPLETETIME%/3600%%60
-echo 转码时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
+print -e \\033[1;36m[PID #0]\\033[0m 转码时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 
-echo 加载速度:	512KB/s
+REM 加载速度:	512KB/s
 set /a COMPLETETIME=%~z1/1024/512
 set /a COMPLETESEC=%COMPLETETIME%%%60
 set /a COMPLETEMIN=%COMPLETETIME%/60%%60
 set /a COMPLETEHOUR=%COMPLETETIME%/3600%%60
-echo 加载时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
+print -e \\033[1;36m[PID #0]\\033[0m 加载时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 
-echo 分解速度:	170KB/s
-set /a COMPLETETIME=%~z1/1024/170
+REM 分解速度:	170KB/s
+set /a COMPLETETIME=%~z1/1024/170*13/4
 set /a COMPLETESEC=%COMPLETETIME%%%60
 set /a COMPLETEMIN=%COMPLETETIME%/60%%60
 set /a COMPLETEHOUR=%COMPLETETIME%/3600%%60
-echo 分解时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
+print -e \\033[1;36m[PID #0]\\033[0m 分解时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
 
-set FILESIZE=0
-set /a FILESIZE=%~z1/1024
-set /a COMPLETETIME=%FILESIZE%/1024/256+%FILESIZE%/1024/128+%FILESIZE%/512+%FILESIZE%/170
-set /a COMPLETESEC=%COMPLETETIME%%%60
-set /a COMPLETEMIN=%COMPLETETIME%/60%%60
-set /a COMPLETEHOUR=%COMPLETETIME%/3600%%60
-echo 约需时间:	%COMPLETEHOUR%时 %COMPLETEMIN%分 %COMPLETESEC%秒
-
+print -e \\033[1;36m[PID #0]\\033[0m 正在配置参数...
 set RAND=%RANDOM%
 set OUTFILE=%~nx1_encode
 set OUTFILE=%OUTFILE: =_%
-if exist "%~dp1%OUTFILE%" rd /s /q "%~dp1%OUTFILE%" >nul
-mkdir "%~dp1%OUTFILE%" >nul
+if not exist "%~dp1%OUTFILE%" mkdir "%~dp1%OUTFILE%" >nul
 
-echo [INFO] 正在编码文件...
+print -e \\033[1;36m[PID #0]\\033[0m 正在编码文件...
 base64 -w235 "%~1" > "%~dp1file%RAND%.tmp"
 
-echo [INFO] 正在转码文件...
+print -e \\033[1;36m[PID #0]\\033[0m 正在转码文件...
 sed -i "s/\//-/g" "%~dp1file%RAND%.tmp"
 
-echo [INFO] 正在计算文件哈希值...
+print -e \\033[1;36m[PID #0]\\033[0m 正在创建校验文件...
 for /f "delims=" %%i in ('certutil -hashfile %1 MD5 ^| findstr /V 哈希 ^| findstr /V 完成') do (cd. > "%~dp1%OUTFILE%\hash_%%i.encode") 
 
-echo [INFO] 正在加载文件...
+print -e \\033[1;36m[PID #0]\\033[0m 正在加载文件...
 set COUNT=0
+
 for /f "delims=" %%i in ('type "%~dp1file%RAND%.tmp"') do (
 	setlocal enabledelayedexpansion
-	set /a "COMPLETESEC=(%FILESCOUNT%-!COUNT!)/1024%%60"
-	set /a "COMPLETEMIN=(%FILESCOUNT%-!COUNT!)/1024/60%%60"
-	set /a "COMPLETEHOUR=(%FILESCOUNT%-!COUNT!)/1024/3600%%60"
+	set /a "COMPLETESEC=(%FILESCOUNT%-!COUNT!)*13/4/1024%%60"
+	set /a "COMPLETEMIN=(%FILESCOUNT%-!COUNT!)*13/4/1024/60%%60"
+	set /a "COMPLETEHOUR=(%FILESCOUNT%-!COUNT!)*13/4/1024/3600%%60"
 	cd. > "%~dp1%OUTFILE%\data%RAND%_!COUNT!_%%i"
-	set /a PER=!COUNT!%%596
-	if "!PER!" == "0" echo [INFO] 正在分解文件...	[!COUNT!/%FILESCOUNT%]	[ETA !COMPLETEHOUR!时!COMPLETEMIN!分!COMPLETESEC!秒]
+	set /a PER=!COUNT!%%71
+	if "!PER!" == "0" print -en \\033[1;36m[PID #!COUNT!]\\033[0m 正在分解文件...	\\033[1;32m[!COUNT!/%FILESCOUNT%]	\\033[1;33m[ETA !COMPLETEHOUR!时!COMPLETEMIN!分!COMPLETESEC!秒] \\033[0m \r
 	endlocal
 	set /a COUNT+=1
 )
-
+print -e \\033[1;36m[PID #%COUNT%]\\033[0m 正在分解文件...	\\033[1;32m[%COUNT%/%COUNT%]	\\033[1;33m[ETA 0时0分0秒] \\033[0m
+print -e \\033[1;36m[PID #0]\\033[0m 正在删除临时文件...
 del /f /q "%~dp1file%RAND%.tmp" >nul
+
+print -e \\033[1;36m[PID #0]\\033[0m 正在创建索引文件...
 set /a COUNT=%COUNT%-1
 cd. > "%~dp1%OUTFILE%\index_%RAND%_%COUNT%.encode"
+
+print -e \\033[1;36m[PID #0]\\033[0m 正在创建文件名...
 set FILEEXT=%~x1
 cd. > "%~dp1%OUTFILE%\filename_%~n1_%FILEEXT:~1%.encode"
-echo [INFO] 分解完成!
-goto :EOF
+
+print -e \\033[1;36m[PID #0]\\033[0m 分解完成!
+goto :END
+
+:END
+set /a END_TIMESTAMP=%time:~0,2%*360000+%time:~3,2%*6000+%time:~6,2%*100+%time:~9,2%
+set /a COST=(%END_TIMESTAMP%-%START_TIMESTAMP%)*10+%RANDOM%%%10
+print -en \\033[1;32m[PID #0]\\033[0m [Process completed (%COST%ms) - press Enter]
+set /p =
